40f0f639041c2532c5f5c3a695f9ec3e5936b34a
IntuzDEVE6 <deve6@intuz.net>
2023-05-02 14:04:41 +0530

    Update stock_picking.py

--- madareem_hotel_extension/models/stock_picking.py
+++ madareem_hotel_extension/models/stock_picking.py
@@ -9,10 +9,14 @@ class StockPiking(models.Model):
     total_cost = fields.Float(string="Total Cost", compute="_compute_product_total_cost")
 
     # This method use for Product Total Cost.
-    @api.depends('move_ids_without_package.product_uom_qty')
+    @api.depends('move_lines')
     def _compute_product_total_cost(self):
         for record in self:
             total = 0.0
-            for line in record.move_ids_without_package:
-                total += line.product_id.standard_price * line.product_uom_qty
+            scraps = self.env['stock.scrap'].search([('picking_id', '=', record.id)])
+            domain = [('id', 'in', (record.move_lines + scraps.move_id).stock_valuation_layer_ids.ids)]
+            valuations = self.env['stock.valuation.layer'].sudo().search(domain)
+            if valuations:
+                for valuation_line in valuations:
+                    total += valuation_line.value
             record.total_cost = total
